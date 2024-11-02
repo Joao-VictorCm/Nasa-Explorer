71a970d1a062b1661063fab17acef530dc193ba5
Joao-VictorCm <joaovictor27cm@gmail.com>
2024-11-02 20:02:54 -0300

    add segunda api com as fotos de marte, e já renderizando na tela

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import axios from "axios"
 
 const app = express()
 const port = 3000
-const API_URL = "https://api.nasa.gov/planetary/apod"
+const apod = "https://api.nasa.gov/planetary/apod"
+const marsPhoto = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&"
 const apiKey = "0PyXWKPSRwnftr7fdJeEqc2RatHhzxGFaXuMXRoN"
+
 
 
 
@@ -18,21 +20,33 @@ app.use(express.static("public", {
 }))
 
 app.get("/", (req, res) =>{
-    res.render("index.ejs")
+    res.render("section.ejs")
 })
 
 
 
 
 app.get("/APOD", async (req, res) => {
-    const response = await axios.get(API_URL,{
+    const response = await axios.get(apod,{
         params:{
             api_key: apiKey
         }
     })
     const result = response.data
+    res.render("apod.ejs", {data: result})
+})
+
+
+
+app.get("/Mars", async (req, res) => {
+    const response = await axios.get(marsPhoto,{
+        params:{
+            api_key: apiKey
+        }
+    })
+    const result = response.data.photos
     console.log(result)
-    res.render("main.ejs", {data: result})
+    res.render("mars.ejs", {data: result})
 })
 
 
